666c95fd8600fc44c0705548e9481151e32771da
Furrya Black <furrya.black@gmail.com>
2025-02-13 15:02:36 +0300

    add comments to main app.js file to explain what happens here

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const { Stack } = require('./Stack');
 
 const stack = new Stack(3);
 
-console.log('stack.limit', stack.limit)
-stack.ping();
-stack.printLimit();
-console.log(Stack.prototype.constructor.name);
+console.log('stack.limit', stack.limit) // stack.limit 3
+
+// Stack method
+stack.ping(); // ping
+
+// Stack object uses Display service injected into the Stack constructor
+stack.printLimit(); // display: limit 3
+
+// Original Stack constructor didn't loose
+console.log(Stack.prototype.constructor.name); // Stack
